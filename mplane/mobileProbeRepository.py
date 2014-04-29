6e6f5d6d21a05ad1335b9909f8f66abb45f67651
ilias <ilias@PC506064.local>
2014-04-29 10:38:10 +0200

    changes

--- mplane/mobileProbeRepository.py
+++ mplane/mobileProbeRepository.py
@@ -50,18 +50,34 @@ def getAvailableDevicesFromDB():
     return parsed
 
 
-def getMeasurementFromDB(from, to, device, collection, parameter):
+def getMeasurementFromDB(fromTs, toTs, device, collection="cellInfo", parameter="currentCellLocation"):
+    #debug (todo:delete)
+    print ("QUERING MONGO DB FOR MEASUREMENTS")
+    print ("DATES  : ", fromTs, " --> ", toTs)
+    print ("DEVICE : ", device)
+    print ("Coll   : ", collection,".", parameter)
+    #init
+    results = []
     client = MongoClient()
     db = client.mPlaneClient
-    devices = db.cellInfo.distinct("properties.deviceID")
-    devices.remove(None)
-    parsed = ",".join(devices)
-    print (parsed)
-    return parsed
+    #query to run
+    query = {}
+    query["properties.deviceID"] = device
+    query["properties."+parameter] =  {"$exists": True}
+    query["properties.date"] = {"$gte": fromTs, "$lte": toTs};
+    #data projection (columns to return)
+    projection =  {"properties.date": 1, "properties."+parameter: 1 } 
+    #perform the query
+    cursor = db[collection].find(query, projection).sort("properties.timeStamp" , 1 )
+    print ("FOUND ", cursor.count(), " measurements")
+    return cursor
+
+
+   
 
 
 def rssi_singleton_capability(devices):
-    cap = mplane.model.Capability(label="connected-sector", when = "past ... now")
+    cap = mplane.model.Capability(label="connected-sector", when = "past ... now", verb = "query")
     cap.add_parameter("source.device", devices)
     cap.add_result_column("time")
     cap.add_result_column("intermediate.link")
@@ -73,79 +89,51 @@ class MobileProbeService(mplane.scheduler.Service):
         if not ( cap.has_parameter("source.device"))  and  (cap.has_result_column("time") and cap.has_result_column("intermediate.link")):
             raise ValueError("capability not acceptable")
         super(MobileProbeService, self).__init__(cap)
+        print ("INIT SERIVICE")
 
+#fixme: what happens when there are no data. 
     def run(self, spec, check_interrupt):
-        # unpack parameters
-        period = spec.when().period().total_seconds()
-        duration = spec.when().duration().total_seconds()
-
-        '''
-        if duration is not None and duration > 0:
-            count = int(duration / period)
-        else:
-            count = None
-
-        if spec.has_parameter("destination.ip4"):
-            sipaddr = spec.get_parameter_value("source.ip4")
-            dipaddr = spec.get_parameter_value("destination.ip4")
-            ping_process = _ping4_process(sipaddr, dipaddr, period, count)
-        elif spec.has_parameter("destination.ip6"):
-            sipaddr = spec.get_parameter_value("source.ip6")
-            dipaddr = spec.get_parameter_value("destination.ip6")
-            ping_process = _ping6_process(sipaddr, dipaddr, period, count)
-        else:
-            raise ValueError("Missing destination")
-
-        # read output from ping
-        pings = []
-        for line in ping_process.stdout:
-            if check_interrupt():
-                break
-            oneping = _parse_ping_line(line.decode("utf-8"))
-            if oneping is not None:
-                print("ping "+repr(oneping))
-                pings.append(oneping)
- 
-
-        # shut down and reap
-        try:
-            ping_process.kill()
-        except OSError:
-            pass
-        ping_process.wait()
-
-        '''
-        # derive a result from the specification
+        # Got a request to retreive measurements. 
+        # Get the request parameters
+        print ("run got called")
+        period = spec.when().datetimes()
+        fromTs = period[0]
+        toTs = period[1]
+        device = spec.get_parameter_value("source.device")
+        
+        # Get the data
+        results = getMeasurementFromDB(fromTs, toTs, device)
+        # Put the results into the specification reply msg
         res = mplane.model.Result(specification=spec)
 
         # put actual start and end time into result
-        #res.set_when(mplane.model.When(a = pings_start_time(pings), b = pings_end_time(pings)))
+        #fixme: null/empty results.
+        numberOfMeasurements = results.count()
+        startTime = results[0]["properties"]["date"]
+        endTime = results[numberOfMeasurements - 1 ]["properties"]["date"]
+        res.set_when(mplane.model.When(a = startTime, b = endTime))
+        #put the data
+        if res.has_result_column("time") and res.has_result_column("intermediate.link"):
+            for i in range(0,  results.count()):
+                result = results[i]
+                date = result["properties"]["date"]
+                value = result["properties"]["currentCellLocation"]
+                res.set_result_value("time", date, i)
+                res.set_result_value("intermediate.link", value, i)
 
-        '''
-        # are we returning aggregates or raw numbers?
-        if res.has_result_column("delay.twoway.icmp.us"):
-            # raw numbers
-            for i, oneping in enumerate(pings):
-                res.set_result_value("delay.twoway.icmp.us", oneping.usec, i)
-            if res.has_result_column("time"):
-                for i, oneping in enumerate(pings):
-                    res.set_result_value("time", oneping.time, i)
-        else:
-            # aggregates. single row.
-            if res.has_result_column("delay.twoway.icmp.us.min"):
-                res.set_result_value("delay.twoway.icmp.us.min", pings_min_delay(pings))
-            if res.has_result_column("delay.twoway.icmp.us.mean"):
-                res.set_result_value("delay.twoway.icmp.us.mean", pings_mean_delay(pings))
-            if res.has_result_column("delay.twoway.icmp.us.median"):
-                res.set_result_value("delay.twoway.icmp.us.median", pings_median_delay(pings))
-            if res.has_result_column("delay.twoway.icmp.us.max"):
-                res.set_result_value("delay.twoway.icmp.us.max", pings_max_delay(pings))
-            if res.has_result_column("delay.twoway.icmp.us.count"):
-                res.set_result_value("delay.twoway.icmp.us.count", len(pings))
-        '''
 
         return res
 
+
+def manually_test_capability():
+    devices = getAvailableDevicesFromDB()
+    svc = MobileProbeService(rssi_singleton_capability("353918050540026,358848043406974,358848047597893,358848043407105,352605059221267,351565050903399,354793051533265,355251056874894,866173010394946,351565054469835,866173010396297,358848047599451,866173010392577,358848047597935,352605059221028,352605059223594"))
+    spec = mplane.model.Specification(capability=svc.capability())
+    spec.set_parameter_value("source.device", "353918050540026")
+    spec.set_when("2013-09-20 ... 2013-10-5")
+    res = svc.run(spec, lambda: False)
+    print(repr(res))
+    print(mplane.model.unparse_yaml(res))
 
 
 # For right now, start a Tornado-based ping server
@@ -154,6 +142,11 @@ if __name__ == "__main__":
 
     #initialize the registry
     mplane.model.initialize_registry()
+
+    ###MANUAL CHECK SHOULD BE NORMALY DISABLED
+    ## manually_test_capability()
+    ## exit()
+    ######
     #create the scheduler 
     scheduler = mplane.scheduler.Scheduler()
     #get devices 
